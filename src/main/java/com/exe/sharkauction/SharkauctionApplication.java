131428b8f2a01b7a2a7ce20320e9d40a91960434
tranthihongphuc <hongphuc0987@gmail.com>
2024-08-24 21:41:48 +0700

    Initial commit

---
+++ src/main/java/com/exe/sharkauction/SharkauctionApplication.java
@@ -0,0 +1,13 @@
+package com.exe.sharkauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkauctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharkauctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exe/sharkauction/SharkauctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.sharkauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharkauctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
