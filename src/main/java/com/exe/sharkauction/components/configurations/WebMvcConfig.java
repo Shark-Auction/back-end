0324f5f77bd78fd88176877ff9e52fa816555bcf
tranthihongphuc <hongphuc0987@gmail.com>
2024-09-13 23:31:58 +0700

    web socket

--- src/main/java/com/exe/sharkauction/components/configurations/WebMvcConfig.java
+++ src/main/java/com/exe/sharkauction/components/configurations/WebMvcConfig.java
@@ -1,10 +1,17 @@
 package com.exe.sharkauction.components.configurations;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -20,9 +27,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false)
                 .maxAge(MAX_AGE_SECS);
     }
+
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedOriginPatterns(Arrays.asList("*"));
+//        config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+//        config.addAllowedHeader("*");
+//        source.registerCorsConfiguration("/**", config);
+//        return source;
+//    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
