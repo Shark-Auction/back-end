42dd6bcde1710ae47f8ded16b64830f93f4f6384
tranthihongphuc <hongphuc0987@gmail.com>
2024-09-26 23:45:48 +0700

    payment

--- src/main/java/com/exe/sharkauction/mappers/IPaymentMapper.java
+++ src/main/java/com/exe/sharkauction/mappers/IPaymentMapper.java
@@ -2,10 +2,13 @@ package com.exe.sharkauction.mappers;
 
 import com.exe.sharkauction.models.PaymentEntity;
 import com.exe.sharkauction.requests.PaymentRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-
+@Mapper(componentModel = "spring")
 public interface IPaymentMapper {
     IPaymentMapper INSTANCE = Mappers.getMapper(IPaymentMapper.class);
-
+    @Mapping(source = "orderId", target = "orderEntity.id")
+    @Mapping(source = "userId", target = "paymentUser.id")
     PaymentEntity toModel(PaymentRequest request);
 }
